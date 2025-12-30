8c55bc19f6a38234c79b7931c30ec2ebb8099e9b
ZiyadCh <chetoziyad@gmail.com>
2025-12-30 15:45:22 +0100

    structure

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+class connection{
+    private $host = "localhost";
+    private $user = "coachuser";
+    private $password = "strongpassword";
+    private $db = "matche";
+    private $pdo;
+
+    protected function connect(){
+        try {
+                    try {
+            $dsn = "mysql:host=$this->host;dbname=$this->db;charset=utf8mb4";
+           $this->pdo = new PDO($dsn, $this->user, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->pdo;
+        } catch (PDOException $e) {
+            error_log("Database connection error: " . $e->getMessage());
+            throw new Exception("Database connection failed");
+        }
+        } catch (\Throwable $th) {
+                        error_log("Database connection error: " . $e->getMessage());
+            throw new Exception("Database connection failed");
+ 
+        }
+    }
+
+  
+}
+
+?>
