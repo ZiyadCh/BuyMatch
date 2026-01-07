ecbab8ce4c1a56016224151aa39a78f495b1e789
ZiyadCh <chetoziyad@gmail.com>
2026-01-07 13:10:01 +0100

    rm pass

--- email_config.php
+++ email_config.php
@@ -2,7 +2,7 @@
 return [
     'smtp_host' => 'smtp.gmail.com',
     'smtp_username' => 'chetoziyad@gmail.com',
-    'smtp_password' => 'ssrv zyrq okzk abat', 
+    'smtp_password' => '', 
     'smtp_port' => 587,
     'smtp_from_email' => 'chetoziyad@gmail.com',
     'smtp_from_name' => 'Buymatch'
