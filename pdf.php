08bc3268ae8d01d5684d50590244ea2b7d19b86d
ZiyadCh <chetoziyad@gmail.com>
2026-01-07 12:56:53 +0100

    email git

--- pdf.php
+++ pdf.php
@@ -5,10 +5,12 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require __DIR__ .'/vendor/autoload.php';
-
+$name = $_SESSION['nom'];
+$prenom = $_SESSION['prenom'];
 $html = "<h1>Ticket de buymatch</h1>
 <div>
-<h3>".$_SESSION['nom']."</h3>
+<h3>$name</h3>
+<h3>$prenom</h3>
 <p>votre ticket</p>
 </div>
 ";
