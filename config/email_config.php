157d57301add05a84f4652b5cd2ad4a23bee93ca
ZiyadCh <chetoziyad@gmail.com>
2026-01-07 11:08:18 +0100

    gitingnore

--- config/email_config.php
+++ config/email_config.php
@@ -2,7 +2,7 @@
 return [
     'smtp_host' => 'smtp.gmail.com',
     'smtp_username' => 'chetoziyad@gmail.com',
-    'smtp_password' => 'YOUR_APP_PASSWORD_HERE', // Get from Google Account settings
+    'smtp_password' => 'YOUR_APP_PASSWORD_HERE', 
     'smtp_port' => 587,
     'smtp_from_email' => 'chetoziyad@gmail.com',
     'smtp_from_name' => 'Ticket System'
